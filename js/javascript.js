92047de03a108dc9c98a676f81133b24fd5f5dd3
MelinaGauna <115503531+MelinaGauna@users.noreply.github.com>
2022-11-13 15:59:34 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+function inclusive() { document.body.style.background = "black";
+                    document.getElementById("section").style.backgroundColor = "black";
+                    document.getElementById("section").style.color = "whitesmoke";
+                    document.getElementById("section1").style.backgroundColor = "black";
+                    document.getElementById("aside").style.color = "whitesmoke";
+                    document.getElementById("aside").style.background = "black";
+                    document.getElementById("footer").style.background = "black";
+                    document.getElementById("footer").style.color = "whitesmoke";
+                    document.getElementById("subtit1").style.color = "whitesmoke";
+                    document.getElementById("subtit2").style.color = "whitesmoke";
+                    document.getElementById("subtit3").style.color = "whitesmoke";
+                    document.getElementById("subtit4").style.color = "whitesmoke";
+                    document.getElementById("subtit5").style.color = "whitesmoke";
+                    document.getElementById("subtit6").style.color = "whitesmoke";
+                    document.getElementById("subtit7").style.color = "whitesmoke";
+}
+
+function copy() { alert("Melina Gauna. FrontEnd. 2022")
+
+}
